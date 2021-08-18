c7750549f4f069945c19728f79dba0fbea74d2fd
utkvermaq <87679491+utkvermaq@users.noreply.github.com>
2021-08-18 22:33:03 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m SKULLRobot
+web: python3 -m SKULLRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE SKULLRobot
+:: Enables virtual env mode and then starts SKULLRobot
+env\scripts\activate.bat && py -m SKULLRobot
